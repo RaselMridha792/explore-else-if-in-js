2c01b2b359ab65e83c5d3f9b5a3fbdf88626428c
rasel mridha <raselmridha792@gmail.com>
2024-08-31 17:54:52 +0600

    exploring nested else if

---
+++ nested.js
@@ -0,0 +1,18 @@
+const money = 40;
+if (money > 400){
+    console.log("you are rich");
+    if(money > 450){
+        console.log("you can buy biff")
+    }
+    else{
+        console.log("you can buy chicken");
+    }
+}
+else{
+    if(money > 300){
+        console.log("you can buy fish");
+    }
+    else{
+        console.log("you can buy eggs");
+    }
+}
